fc1fcf0a6aebaf7d96b7dbe980982bdf2cebb869
Long3112 <128303674+Long3112@users.noreply.github.com>
2024-06-03 19:15:17 +0700

    Update script1.js
    new change

--- User/Conno/script1.js
+++ User/Conno/script1.js
@@ -15,6 +15,23 @@ searchInput.addEventListener('input', function() {
     }
   }
 });
+const table1 = document.querySelector('#customers_table table');
+const searchInput1 = document.querySelector('#customers_table .input-group input');
+
+searchInput.addEventListener('input', function() {
+  const searchTerm = this.value.toLowerCase();
+  const rows = table.getElementsByTagName('tr');
+
+  for (let i = 1; i < rows.length; i++) {
+    const amountCell = rows[i].getElementsByTagName('td')[2]; // Chọn ô của cột số tiền (index 2)
+    const amount = amountCell.textContent.toLowerCase();
+    if (amount.includes(searchTerm)) {
+      rows[i].style.display = '';
+    } else {
+      rows[i].style.display = 'none';
+    }
+  }
+});
 document.getElementById('backButton').addEventListener('click', function() {
   // Chuyển hướng về trang index.html
   window.location.href = 'file:///C:/Users/thats/Downloads/Nợ%20xấu/User/user.html';
